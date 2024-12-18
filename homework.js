3a16a53a3b30a1f133a2fd6a8bea1bdaa614f335
MaiGalili <maigalili@gmail.com>
2024-12-18 14:58:43 +0200

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,28 @@
+//import all the moudle
+const fs = require("fs");
+const path = require("path");
+
+//take the path of the text
+const ditPath = path.join(__dirname, "/text"); // take the path all way to folder
+let str = "";
+//run all the file and one by one and print
+for (let i = 1; i <= 10; i++) {
+  //take Each text.txt
+  const textIn = fs.readFileSync(ditPath + "/text" + i + ".txt", "utf-8");
+  const arr = textIn.split("\n");
+
+  //cheack and add the String
+  for (let j = 0; j < i; j++) {
+    if (i < arr.length) {
+      str += arr[j];
+      str += "\n";
+    }
+
+    if (i - 1 > j && arr.length <= i) {
+      str += arr[j];
+      str += "\n";
+    }
+  }
+  // add how the string to new text.txt
+  fs.writeFileSync(ditPath + "/textRead.txt", str);
+}
